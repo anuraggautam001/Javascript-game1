264a160d983a3e4bfd37a1b75b0f607d71f6e3b2
Anurag <anuraggautam876@gmail.com>
2022-02-28 14:31:40 +0530

    Learning js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var scores, roundScore, activePlayer;
 
 scores=[0,0];
 roundScore=0;
-activePlayer=1;
+activePlayer=0;
 
 document.querySelector('.dice').style.display = 'none';
  
@@ -11,7 +11,7 @@ document.getElementById('score--1').textContent='0';
 document.getElementById('current--0').textContent='0';
 document.getElementById('current--0').textContent='0';
 
-document.querySelector('.btn btn--roll').addEventListener('click', function(){
+document.querySelector('.btn--roll').addEventListener('click', function(){
     // 1. random number
     var dice= Math.floor(Math.random() * 6)+1;
 
@@ -22,7 +22,28 @@ document.querySelector('.btn btn--roll').addEventListener('click', function(){
     
 
     //3. Update the round score If the rolled number was not a 1
+     if(dice !== 1){
+         //Add Score
+        roundScore+=dice;  
+        document.querySelector('#current--'+activePlayer).textContent = roundScore;
 
+     }
+     else{
+         //next Player
+         activePlayer === 0 ? activePlayer=1:activePlayer=0;
+         roundScore=0;
+
+         document.getElementById('current--0').textContent= '0';
+         document.getElementById('current--1').textContent= '0';
+
+         document.querySelector('.player--0 player--active').classList.toggle('avtive');
+         document.querySelector('.player--1').classList.toggle('active');
+
+        //  document.querySelector('.player--0 player--active').classList.remove('avtive');
+        //  document.querySelector('.player--1').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+     }
 });
 
 
